0b9f423f8eb73b22c97fa7731c13c10332b90e34
Lucas Davi Ferreira de Faria <lucas.faria1@gmail.com>
2023-05-13 21:29:03 -0300

    fix: testing this fix in cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-useless-escape */
 import cors from 'cors';
 
-const allowedOrigins = [/\dev.faithpoint.us$/, /\homolocagacao.faithpoint.us$/, /\faithpoint.us$/];
+const allowedOrigins = [/\.dev\.faithpoint\.us$/, /\.homolocagacao\.faithpoint\.us$/, /\.faithpoint\.us$/];
 
 const options: cors.CorsOptions = {
-	origin: allowedOrigins,
+	origin: allowedOrigins
 };
 
 export default options;
