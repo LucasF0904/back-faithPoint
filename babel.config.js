ac26ca25021fd1b5cab21c69b2948c177d4120f7
Lucas Davi Ferreira de Faria <lucas.faria1@gmail.com>
2023-05-13 22:51:14 -0300

    fix: testing deploy

--- babel.config.js
+++ babel.config.js
@@ -17,6 +17,7 @@ module.exports = {
 				},
 			},
 		],
+		'@babel/plugin-transform-flow-strip-types',
 		'babel-plugin-transform-typescript-metadata',
 		['@babel/plugin-proposal-decorators', { legacy: true }],
 		['@babel/plugin-proposal-class-properties', { loose: true }],
